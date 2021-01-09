e9aa6f907a4ce306f7733d5e07bdcb4476b56ba2
Porya <theporyafm12@gmail.com>
2021-01-08 21:29:17 -0800

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,6 +1,7 @@
 const prefix = require('../models/Prefix');
 
 module.exports = async (bot, message) => {
+  if(message.author.bot) return;
   bot.snipes.set(message.channel.id, {
     content: message.content,
     author: message.author.tag,
